2328bbc7c152cdc0faa293f988840bf7e07e3fea
Eli <eli@Razas-Air.localdomain>
2020-03-02 12:00:53 -0800

    random position button

--- src/Snack2.js
+++ src/Snack2.js
@@ -1,12 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 
 function Snack2() {
+  const [positionsArray, setPositionsArray] = useState([]);
+
+  function makePosition(){
+    const x = Math.random()*100;
+    const y = Math.random()*100;
+    setPositionsArray([...positionsArray, {x, y}]);
+  }
 
   return (
     <div>
       <h1>XX Hot Cheetos</h1>
-      <img src='https://www.cheetos.com/sites/cheetos.com/files/styles/product_thumbnail/public/2019-03/XXXtra%20Flamin%20Hot%20Crunchy_v2.png?itok=KdBZz32r' className='App-logo' alt='xx hot cheetos'></img>
+      {
+        positionsArray.map((position) => {
+
+          const styleObject = {
+            position: "fixed",
+            left: `${position.x}%`,
+            top: `${position.y}%`
+          }
+
+          return <img src='https://www.cheetos.com/sites/cheetos.com/files/styles/product_thumbnail/public/2019-03/XXXtra%20Flamin%20Hot%20Crunchy_v2.png?itok=KdBZz32r'
+            className='App-logo'
+            alt='xx hot cheetos' style={styleObject} />
+        }
+        )
+      }
+      <button onClick={makePosition}> Add a bag!</button>
       <Link to='/'>
         <button>Back to Vending Machine</button>
       </Link>
